75f4d3cdd003b552c774374af1b7360dcbaadfe1
shwetzpatil <shwetzpatil@gmail.com>
2018-08-20 13:46:01 +0000

    checking for string input

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,13 @@
 def fizzbuzz(number)
-    if number == 3
-        "fizz"
-    elsif number == 5
-     "buzz"
-    else number == 15
-     "fizzbuzz"
-    end
+  if number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 5 == 0
+    "buzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number.class == String  
+    nil
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,19 @@ describe "fizzbuzz" do
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end
     
+    it "require 'fizz' when passes 9" do
+        expect(fizzbuzz(9)).to eq 'fizz'
+    end
+    it "require 'fizzbuzz' when passes 10" do
+        expect(fizzbuzz(10)).to eq 'buzz'
+    end
+    it "require 2 when passes 2" do
+        expect(fizzbuzz(2)).to eq 2
+    end
+    it "require nil when passes 'xyz'" do
+        expect(fizzbuzz('xyz')).to eq nil
+    end
+    it "require 2.12 when passes 2.12" do
+        expect(fizzbuzz(2.12)).to eq 2.12
+    end
 end
