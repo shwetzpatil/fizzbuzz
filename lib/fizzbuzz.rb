1c658bb8930a85f228622fd9374fcf57ce91a77e
shwetzpatil <shwetzpatil@gmail.com>
2018-08-20 12:55:31 +0000

    minimum tests checked

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(number)
-    "fizz"
+    if number == 3
+        "fizz"
+    elsif number == 5
+     "buzz"
+    else number == 15
+     "fizzbuzz"
+    end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,13 @@ describe "fizzbuzz" do
     it "requires 'fizz' when passed 3" do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
+     
+    it "require 'buzz' when passed 5" do 
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    
+    it "require 'fizzbuzz' when passes 15" do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+    
 end
